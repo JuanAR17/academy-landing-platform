9a4e44c784c419c6757c35e7bad4f8766361c806
GrenRadon <julianeduardovillamizarpena_14021994@outlook.com>
2025-10-02 23:31:52 +0000

    minimal changes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // Pon aquí el dominio del backend que usarás detrás del proxy
   // (alineado con BACKEND_HOST en tu .env de docker-compose)
-  apiBase: 'https://api.lacademia.com.co'
+  apiBase: 'https://api.iacademia.com.co'
 };
 
