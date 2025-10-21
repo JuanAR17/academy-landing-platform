43d398e00eccb8fd4a98ecd4bad8ded8e3cedd55
julian <julianeduardovillamizarpena_14021994@outlook.com>
2025-10-21 13:34:45 -0500

    Cambiando variables en el .env y arreglando problema de cors para testing

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:18081/api',
+  apiUrl: '/api',
   localData: 'assets/data',
-  epaycoAPiUrl: 'https://apify.epayco.co',
-
-  PUBLIC_EPAYCO_KEY : '2975d07f07d14780d0707294476cb51d',
-  PRIVATE_EPAYCO_KEY: '2cdd56d6d562b72556ea2cff53557839'
 };
 
